c9cf99d268eec19dee3613bbc8047f58915169a8
Zestinc <zestinc32@gmail.com>
2018-06-10 16:10:53 -0700

    ????

--- app/src/main/java/com/example/zestinc/airhockey/util/Geometry.java
+++ app/src/main/java/com/example/zestinc/airhockey/util/Geometry.java
@@ -4,6 +4,7 @@ package com.example.zestinc.airhockey.util;
 public class Geometry {
     public static class Point {
         public final float x, y, z;
+        public Point(float x, float y, float z) {
             this.x = x;
             this.y = y;
             this.z = z;
